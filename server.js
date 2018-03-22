3b48464174d4a67e9c3f0fe1c9e2c7182da1ba92
G P <giulio.pietroiusti@gmail.com>
2018-03-22 20:14:37 +0100

    Accept only GET requests

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 "use strict";
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const mongodb = require('mongodb');
 
@@ -14,5 +15,25 @@ mongodb.MongoClient.connect(process.env.DBURI, (err, client) => {
     var collection = client.db(process.env.DBNAME).collection('usersRequests');
 
     console.log('connected to db');
+
+    const server = http.createServer((req, res) => {
+	req.on('error', (err) => {
+	    console.log(err);
+	    res.statusCode = 400;
+	    res.end();
+	});
+
+	if (req.method === 'GET') {
+	    // TODO
+	    console.log('GET');
+	} else {
+	    res.statusCode = 404;
+	    res.end('Wrong method');
+	}
+    });
+
+    server.listen(port, () => {
+	console.log('Listening at port ' + port);
+    });
         
 });
