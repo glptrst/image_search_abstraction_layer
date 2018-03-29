3cc9e01074363f652df72648a199c3a93c0e44a7
G P <giulio.pietroiusti@gmail.com>
2018-03-29 15:48:43 +0200

    Insert user query into db.

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ mongodb.MongoClient.connect(process.env.DBURI, (err, client) => {
 	    } else if (splittedUrl[1] === 'api' && splittedUrl[2] === 'imagesearch') {
 
 		var query = splittedUrl[3];
-		// TODO: handle offstring parameter
+		// TODO: handle offset parameter
 
 		//sample request
 		var url = 'https://www.googleapis.com/customsearch/v1?q=' + query + '&cx=' + process.env.CX + '&searchType=image&key=' + process.env.API_KEY;
@@ -55,11 +55,25 @@ mongodb.MongoClient.connect(process.env.DBURI, (err, client) => {
 		    });
 		    response.on('end', () => {
 			try {
-			    //console.log(JSON.parse(rawData));
+			    // console.log(JSON.parse(rawData));
 			    // Show fetched data to the user
 			    res.statusCode = 200;
 			    res.setHeader('Content-type', 'application/json');
 			    res.end(rawData);
+
+			    // Insert query in db
+			    const userQuery = {
+				"query": query,
+				"time": new Date().getTime()
+			    };
+			    collection.insertOne(userQuery, function(err, result){
+				if (err) {
+				    console.log(err);
+				}
+				else {
+				    console.log('Request stored in db');
+				}
+			    });
 			} catch (e) {
 			    console.error(e.message);
 			}
